4d668c5708e3196d73418f9925399c97996e9007
David Orlando Miranda <domirandar@unal.edu.co>
2023-08-07 18:43:43 -0500

    fetch

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,12 +1,11 @@
 $(function() {
+    // ...
 
     $("input,textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
+        // ...
+
         submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
+            event.preventDefault(); // prevent default submit behavior
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -17,17 +16,23 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
+
+            // Use fetch to make the POST request to the AWS API
+            fetch('https://j1mxvebrqe.execute-api.sa-east-1.amazonaws.com/Prod/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
                 },
-                cache: false,
-                success: function() {
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    phone: phone,
+                    message: message
+                }),
+                credentials: 'include' // O 'same-origin' si tu frontend y API están en el mismo dominio
+            })
+            .then((response) => {
+                if (response.ok) {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -37,32 +42,31 @@ $(function() {
                     $('#success > .alert-success')
                         .append('</div>');
 
-                    //clear all fields
+                    // Clear all fields
                     $('#contactForm').trigger("reset");
-                },
-                error: function() {
+                } else {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
-                    //clear all fields
+
+                    // Clear all fields
                     $('#contactForm').trigger("reset");
-                },
+                }
             })
+            .catch((error) => {
+                // Error handling
+                console.error('Error:', error);
+            });
         },
-        filter: function() {
-            return $(this).is(":visible");
-        },
+
+        // ...
     });
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
+    // ...
 });
-
 
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
