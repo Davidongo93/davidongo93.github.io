39ca3664f645e20e3327b5a3ce4925b382475e9f
David Orlando Miranda <domirandar@unal.edu.co>
2023-08-19 00:06:38 -0500

    gtag OK

--- js/gtag.js
+++ js/gtag.js
@@ -1,10 +1,5 @@
 window.addEventListener('load', function() {
   try {
-    var script = document.createElement('script');
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-5Q8HLX9RJ1";
-    script.async = true;
-    document.head.appendChild(script);
-
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
